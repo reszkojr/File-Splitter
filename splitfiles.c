e447f2d961245c4724ea8ad683b943fb089cd046
reszkojr <s.reszkojr@gmail.com>
2022-01-06 19:56:05 -0300

    Odd size in megabytes bug fixed. Now if the file to be splitted has an odd size in megabytes, it will create the right amount of files.

--- splitfiles.c
+++ splitfiles.c
@@ -1,8 +1,8 @@
 /* 
     Program's logic
 
-    1. You put in the function "split_files" parameter the file that you want
-    to split, and the size that each file will be splitted.
+    You put in the "split_files" function parameter the file that you want
+    to split, and the size in megabytes that each file will be splitted.
 
     Imagine that the file to be split has 32MB and we want to split it into 
     segments of 8MB each. The amount of segments that will exist is gonna be
@@ -15,36 +15,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 int get_file_size(FILE *file);
-int split_file(FILE *file, int megabytes); // This function receives a file to be split and the megabytes that each segment will have
+int split_file(FILE *file, float megabytes); // This function receives a file to be split and the megabytes that each segment will have
 
 int main(void) {
     FILE *file = fopen("testfile.txt", "r"); // Opens the "textfile.txt" in read-only mode.
     // printf("\"textfile.txt\" size in bytes is %d.\n", get_file_size(file));
 
-    split_file(file, 2);
+    split_file(file, 3);
 }
 
-int split_file(FILE *file, int megabytes) {
+int split_file(FILE *file, float megabytes) {
 
     long segments_size = megabytes * pow(1024, 2);      // Size of each segment in bytes
     long file_size = get_file_size(file);               // File size in bytes
-    
-    int number_of_segments = file_size / segments_size; // Number of segments to be created
 
+    float file_size_mb = file_size / pow(1024, 2);        // File size in megabytes
+
+    /*
+        We use the file size in megabytes to calculate 
+        the remainder of the division between it and 
+        the segments size in megabytes.
+    */
+    
+    int number_of_segments = ceil(file_size_mb / megabytes);
+
+    // Number of segments to be created
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
     if (file_size < segments_size) return -1;           // If the file is smaller than the segments size, return
 
-    for (int i = 1; i <= number_of_segments; i++) {
+    fseek(file, 0, SEEK_END);                           // Makes the stream (cursor) go to the end of the file
+
+    for (int i = 1; i <= number_of_segments; i++) {     // This for loop creates and split the files for us
         char num[3];
         char segment_name[11] = "split_";
-        sprintf(num, "%d", i);                      // Stores the "i" integer into the "num" string
+        sprintf(num, "%d", i);                          // Stores the "i" integer into the "num" string
         strcat(segment_name, num);                      // Concatenates the "segment_name" with "num"
 
         printf("%s\n", segment_name);
 
-        FILE *file = fopen(segment_name, "w");          // Creates the segment file number N, being N i
-
+        FILE *segment_file = fopen(segment_name, "w");          // Creates the segment file number N, being N i
     }
 }
 
